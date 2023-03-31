a542338d2f4736ef9094fa46bee4c40b133cde5e
brandohuaman <42688074+brandohuaman@users.noreply.github.com>
2023-03-31 10:47:29 -0500

    Update Basic Join Top Competitors.sql

--- HackerRank/Basic Join Top Competitors.sql
+++ HackerRank/Basic Join Top Competitors.sql
@@ -4,6 +4,7 @@ the leaderboard! Write a query to print the respective hacker_id and name of hac
 who achieved full scores for more than one challenge. Order your output in descending
 order by the total number of challenges in which the hacker earned a full score. 
 If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+Link: https://www.hackerrank.com/challenges/full-score/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
 */
 
 SELECT S.hacker_id, name
