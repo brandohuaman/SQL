c45c058a970d4ec0ee498338d73d102132d36896
brandohuaman <42688074+brandohuaman@users.noreply.github.com>
2023-04-08 11:12:52 -0500

    Create Sales Analysis III (Uniqueness).sql

---
+++ LeetCode/Sales Analysis III (Uniqueness).sql
@@ -0,0 +1,42 @@
+/*
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table has no primary key, it can have repeated rows.
+product_id is a foreign key to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+
+Return the result table in any order.
+Link: https://leetcode.com/problems/sales-analysis-iii/description/?envType=study-plan&id=sql-i
+*/
+--We use MIN and MAX functions in order to get the uniqueness of products sold in the Q1 that haven't been sold in any other period. 
+SELECT s.product_id, product_name
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY s.product_id
+HAVING MIN(sale_date) >= CAST('2019-01-01' AS DATE) AND
+       MAX(sale_date) <= CAST('2019-03-31' AS DATE)
