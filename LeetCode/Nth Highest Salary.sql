989194f38b5a09870de8bf11aac97187504fa7fb
brandohuaman <42688074+brandohuaman@users.noreply.github.com>
2023-04-08 21:14:37 -0500

    Update Nth Highest Salary.sql

--- LeetCode/Nth Highest Salary.sql
+++ LeetCode/Nth Highest Salary.sql
@@ -17,12 +17,11 @@ Link: https://leetcode.com/problems/nth-highest-salary/description/
 
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-DECLARE P INT;
-SET P=N-1;
+SET N=N-1;
   RETURN (
       # Write your MySQL query statement below.
      SELECT DISTINCT(salary) FROM Employee 
-     ORDER BY salary
-     LIMIT P, 1
+     ORDER BY salary DESC
+     LIMIT 1 OFFSET N
   );
 END
